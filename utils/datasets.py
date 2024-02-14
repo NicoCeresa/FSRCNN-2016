a4dfee75c6d0c6c4a1bbe0d8d114f66555f96a17
Nico Ceresa <ceresanico@gmail.com>
2024-02-14 13:47:31 -0800

    removed scale initialization to 2

--- utils/datasets.py
+++ utils/datasets.py
@@ -11,7 +11,7 @@ from PIL import Image
 
 class TrainDIV2K(Dataset):
     
-    def __init__(self, dir_path:str, scale=2):
+    def __init__(self, dir_path:str, scale:int):
         super().__init__()
         # get images from a path
         self.paths = list(pathlib.Path(dir_path).glob(f"*.png"))
@@ -58,7 +58,7 @@ class TrainDIV2K(Dataset):
 
 class EvalDIV2K(Dataset):
     
-    def __init__(self, dir_path:str, scale=2):
+    def __init__(self, dir_path:str, scale:int):
         super().__init__()
         # get images from a path
         self.paths = list(pathlib.Path(dir_path).glob(f"*.png"))
@@ -103,35 +103,35 @@ class EvalDIV2K(Dataset):
         LR, HR = self.test_transform(img)
         return torch.Tensor(np.expand_dims(LR / 255., 0)).squeeze(0), torch.Tensor(np.expand_dims(HR / 255., 0)).squeeze(0)
     
-    if __name__ == '__main__':
-        PROJECT_PATH = 'G:/Projects/FSRCNN-2016/'
-        project_directory = Path(PROJECT_PATH)
+if __name__ == '__main__':
+    PROJECT_PATH = 'G:/Projects/FSRCNN-2016/'
+    project_directory = Path(PROJECT_PATH)
 
-        zip_data_path = Path(os.path.join(project_directory, 'zips'))
-        image_data_path = Path(os.path.join(project_directory, 'images'))
+    zip_data_path = Path(os.path.join(project_directory, 'zips'))
+    image_data_path = Path(os.path.join(project_directory, 'images'))
 
-        train_zip_path =  Path(os.path.join(zip_data_path, 'DIV2K_train_HR.zip'))
-        train_image_path =  Path(os.path.join(image_data_path, 'train_images'))
+    train_zip_path =  Path(os.path.join(zip_data_path, 'DIV2K_train_HR.zip'))
+    train_image_path =  Path(os.path.join(image_data_path, 'train_images'))
 
-        valid_zip_path = Path(os.path.join(zip_data_path, 'DIV2K_valid_HR.zip'))
-        valid_image_path = Path(os.path.join(image_data_path, 'valid_images'))
-        
-        # Unzip images to location in env
-        unzip(train_zip_path, train_image_path)
-        unzip(valid_zip_path, valid_image_path)
-        
-        # Put Augmented (rotated and scaled) images in diff folder
-        augmented_image_path = Path(os.path.join(project_directory, 'augmented_images'))
-        if augmented_image_path.is_dir():
-                print(f"{augmented_image_path} already exists")
-        else:
-            augmented_image_path.mkdir(parents=True, exist_ok=True)
-        
-        
-        augment_images(in_path=train_image_path,
+    valid_zip_path = Path(os.path.join(zip_data_path, 'DIV2K_valid_HR.zip'))
+    valid_image_path = Path(os.path.join(image_data_path, 'valid_images'))
+    
+    # Unzip images to location in env
+    unzip(train_zip_path, train_image_path)
+    unzip(valid_zip_path, valid_image_path)
+    
+    # Put Augmented (rotated and scaled) images in diff folder
+    augmented_image_path = Path(os.path.join(project_directory, 'augmented_images'))
+    if augmented_image_path.is_dir():
+            print(f"{augmented_image_path} already exists")
+    else:
+        augmented_image_path.mkdir(parents=True, exist_ok=True)
+    
+    
+    augment_images(in_path=train_image_path,
+            out_path=augmented_image_path,
+            subclass='train')
+
+    augment_images(in_path=valid_image_path,
                 out_path=augmented_image_path,
-                subclass='train')
-
-        augment_images(in_path=valid_image_path,
-                    out_path=augmented_image_path,
-                    subclass='valid')
+                subclass='valid')
